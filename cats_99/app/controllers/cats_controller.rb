0af61232c2e2c3e80799af07bcd21e4b78f341b9
kaizhu94 <zhukai0803@gmail.com>
2020-12-05 10:40:24 -0500

    Update cat_controller

--- cats_99/app/controllers/cats_controller.rb
+++ cats_99/app/controllers/cats_controller.rb
@@ -1,21 +1,23 @@
 class CatsController < ApplicationController
     def index
-        @cats = Cat.all
 
+        @cats = Cat.all
         render :index
+
     end 
 
     def show
 
         @cat = Cat.find(params[:id])
-
         render :show
 
     end
 
     def new
+
         @cat = Cat.new
         render :new
+
     end
 
     def create
@@ -23,9 +25,24 @@ class CatsController < ApplicationController
         if @cat.save
             redirect_to cat_url(@cat)
         else
-            render json: @cat.errors.full_messages, status: 422
-            # flash.now[:errors] = @cat.errors.full_messages
-            # render :new
+            # render json: @cat.errors.full_messages, status: 422
+            flash.now[:errors] = @cat.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            flash.now[:errors] = @cat.errors.full_messages
+            render :edit
         end
     end
 
